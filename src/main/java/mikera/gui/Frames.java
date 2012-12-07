ec9693f2a5d1e66c99007fd519f959bae95f4dc8
mikera <mike.r.anderson.13@gmail.com>
2012-12-08 05:46:58 +0800

    Better frame creation

--- src/main/java/mikera/gui/Frames.java
+++ src/main/java/mikera/gui/Frames.java
@@ -2,6 +2,7 @@ package mikera.gui;
 
 import java.util.HashMap;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
@@ -9,10 +10,34 @@ public class Frames {
 	
 	private static final HashMap<String,JFrame> frames=new HashMap<String, JFrame>();
 
+	public static JFrame createFrame(String title) {
+		JFrame f=frames.get(title);
+		
+		if (f==null) {
+			f=new JFrame(title);
+			frames.put(title,f);
+
+			f.setVisible(true);
+			f.pack();
+			
+			f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		} else {
+			f.getContentPane().removeAll();
+			if (!f.isVisible()) {
+				f.setVisible(true);
+			} else {
+				f.validate();
+			}
+			f.repaint();
+		}
+		
+		return f;
+	}
 	
 	public static JFrame display(final JComponent component) {
-		JFrame f=new JFrame("Image popup");
-		f.add(component);
+		JFrame f=createFrame("View Popup");
+		
+		f.getContentPane().add(component);
 		f.setVisible(true);
 		f.pack();
 		
@@ -22,29 +47,17 @@ public class Frames {
 	}
 	
 	public static JFrame display(final JComponent component, String title) {
-		JFrame f=frames.get(title);
-		
-		if (f==null) {
-			f=new JFrame(title);
-			frames.put(title,f);
-			f.getContentPane().add(component);
-
-			f.setVisible(true);
-			f.pack();
-			
-			f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		} else {
-			f.getContentPane().removeAll();
-			f.getContentPane().add(component);
-			if (!f.isVisible()) {
-				f.setVisible(true);
-			} else {
-				f.validate();
-			}
-			f.repaint();
-		}
+		JFrame f=createFrame(title);
+		f.getContentPane().add(component);
+		if (!f.isVisible()) f.setVisible(true);
+		f.pack();
 				
 		return f;
+	}
+	
+	public static void main(String[] args) {
+		display(new JButton("Hello!"),"Test Frame");
+		display(new JButton("Hello Two!"),"Test Frame");
 	}
 
 }
