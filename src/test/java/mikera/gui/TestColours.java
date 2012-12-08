a6a18bf31084b546017844b59c8ada1f4430e1a7
mikera <mike.r.anderson.13@gmail.com>
2012-12-08 10:40:23 +0800

    Test clamping

--- src/test/java/mikera/gui/TestColours.java
+++ src/test/java/mikera/gui/TestColours.java
@@ -4,11 +4,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import mikera.image.Colours;
+import mikera.util.Maths;
 
 public class TestColours {
 	
 	@Test
 	public void testClamping() {
+		assertEquals(20,Maths.bound(0,20,40));
+		
 		assertEquals(0xFF00FF80, Colours.getARGBClamped(-10, 300, 128, 255));
 	}
 }
