02682e867c6239e5d21b7ab8e20918efbb839941
mikera <mike.r.anderson.13@gmail.com>
2013-09-26 09:38:57 +0800

    add mouse query functions

--- src/main/java/mikera/gui/Tools.java
+++ src/main/java/mikera/gui/Tools.java
@@ -1,6 +1,9 @@
 package mikera.gui;
 
 import java.awt.Dimension;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
 import java.awt.Toolkit;
 
 
@@ -9,5 +12,17 @@ public class Tools {
 	public static Dimension getScreenSize() {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		return dim;
+	}
+	
+	public static Point getMouseLocation() {
+		return MouseInfo.getPointerInfo().getLocation();
+	}
+	
+	public static int getMouseX() {
+		return getMouseLocation().x;
+	}
+	
+	public static int getMouseY() {
+		return getMouseLocation().y;
 	}
 }
